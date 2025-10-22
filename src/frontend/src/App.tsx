6caa34ecf6a0b03287771c6351797a6433dcfeba
Jorg Clausn <100955285+clausn1@users.noreply.github.com>
2025-10-22 18:27:21 +0200

    cambiamos el puerto del backendo

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -11,7 +11,7 @@ type Article = {
 }
 
 // Base de API (para pintar la imagen devuelta por /articles/:id)
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080'
 
 function App() {
   // ---- Estados comunes
